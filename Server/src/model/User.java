3bd86977b0277c83f3a9db2145e9bc4cc7f74f7d
chooseanother <kim.tranberg@gmail.com>
2021-04-07 11:56:48 +0200

    user now contains password, getters for name, password and balance. setter for balance

--- Server/src/model/User.java
+++ Server/src/model/User.java
@@ -1,18 +1,36 @@
 package model;
 
-import java.util.ArrayList;
 
-public class User implements Runnable {
+
+public class User{
     private StocksUserOwns stocks;
     private int balance;
     private String name;
+    private String password;
     private Orders orders;
 
-    public User(String name, int balance) {
-        this.orders = new Orders();
-        this.balance = balance;
-        this.stocks = new StocksUserOwns(name);
+    public User(String name, String password) {
         this.name = name;
+        this.password = password;
+        orders = new Orders();
+        balance = 0;
+        stocks = new StocksUserOwns(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
     }
 
     public void Buy(Stock stock, int amount) {
@@ -47,7 +65,7 @@ public class User implements Runnable {
     }
 
 
-    public void addOrderToBuy(Stock stock, int amount, int price) throws Exception {
+    public void addOrderToBuy(Stock stock, int amount, int price) {
         try {
             Stock stock1;
             if (orders.AddOrderToBuy(stock1 = new Stock(stock.getName(), price, amount)) != null) {
@@ -74,12 +92,4 @@ public class User implements Runnable {
                 ", name='" + name + '\'' +
                 '}';
     }
-
-    @Override
-    public void run() {
-
-
-    }
-
-
 }
