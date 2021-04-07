a0243ca18d194e04a9327c6043864a99f98ba37c
chooseanother <kim.tranberg@gmail.com>
2021-04-07 11:55:58 +0200

    Added login and register user

--- Server/src/model/ModelManger.java
+++ Server/src/model/ModelManger.java
@@ -1,13 +1,20 @@
 package model;
 
+import java.beans.PropertyChangeSupport;
+import java.io.IOException;
+
 public class ModelManger implements Model {
     private Orders orders;
     private User user;
     private Stocks stocks;
+    private UserList userList;
+//    private PropertyChangeSupport property;
 
     public ModelManger() {
+//        this.property = new PropertyChangeSupport(this);
+        userList = new UserList();
         orders = new Orders();
-        user = new User("bob", 1500);
+        user = new User("bob", "123");
         stocks = new Stocks("Market");
         stocks.addStock(new Stock("Apple", 5, 5));
         stocks.addStock(new Stock("Microsoft", 5, 5));
@@ -30,4 +37,19 @@ public class ModelManger implements Model {
     public void PlaceOrdertoBuy(Stock stock, int amount, int price) throws Exception {
         user.addOrderToBuy(stock, amount, price);
     }
+
+    @Override
+    public boolean login(String usr, String pwd) throws Exception {
+        boolean result = userList.login(usr, pwd);
+//        property.firePropertyChange("Login",usr,"Success");
+        return result;
+    }
+
+    @Override
+    public boolean registerUser(String usr, String pwd) throws Exception {
+        boolean result = userList.addProfile(usr, pwd);
+//        property.firePropertyChange("Register",usr,"Success");
+        return result;
+    }
+
 }
