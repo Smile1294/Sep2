8afee78f66b3e92f5f51ebcd3103b6fdd5cdcce2
chooseanother <kim.tranberg@gmail.com>
2021-04-12 19:39:53 +0200

    removed, only needed for testing

--- Server/src/mediator/TestAPIKeySwitching.java
+++
@@ -1,9 +0,0 @@
-package mediator;
-
-public class TestAPIKeySwitching {
-    public static void main(String[] args) {
-        for (int x=0;x<20;x++) {
-            System.out.println(APIKey.getKey());
-        }
-    }
-}
