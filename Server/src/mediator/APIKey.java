65be948dc26f9f8b03ba056464d6102982e91fa0
chooseanother <kim.tranberg@gmail.com>
2021-04-12 21:46:30 +0200

    just switching up what index to start at when testing

--- Server/src/mediator/APIKey.java
+++ Server/src/mediator/APIKey.java
@@ -9,7 +9,7 @@ package mediator;
 public class APIKey {
     private static final String[] KEYS = {"1Z19I81E7YM3VHW9","JI2Z4NFER4CQ52LP","0K7TBQTPK03XHNB0","TD4H3494BLXYYBE1",
             "408T2WU2BSBT1EP1","GB6KGRHXGIJNGXU5"};
-    private static int index = 1;
+    private static int index = 4;
 
     public static String getKey(){
         if (index>KEYS.length-1){
