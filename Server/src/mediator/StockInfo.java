9d8b506d49511c7a9b4ea45c46ddeb9c3a2ab26f
chooseanother <kim.tranberg@gmail.com>
2021-04-12 19:57:33 +0200

    Now can convert from map to array that contains date time

--- Server/src/mediator/StockInfo.java
+++ Server/src/mediator/StockInfo.java
@@ -2,36 +2,52 @@ package mediator;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Map;
 
 public class StockInfo {
     @SerializedName("Meta Data")
     private MetaData metaData;
     @SerializedName("Time Series (60min)")
-    private Map<String,TradingData> timeSeries;
+    private Map<String,TradingData> tradingDataMap;
 
-//    @SerializedName("Time Series (5min)")
-//    private Map<Date,TradingData> timeSeries;
+    private ArrayList<TradingData> timeSeries;
 
-    public StockInfo(MetaData metaData, Map<String,TradingData> timeSeries) {
+    public StockInfo(MetaData metaData, Map<String,TradingData> tradingDataMap) {
         this.metaData = metaData;
-        this.timeSeries = timeSeries;
+        this.tradingDataMap = tradingDataMap;
+        timeSeries = null;
+    }
+
+    public StockInfo convert(){
+        timeSeries = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+        for (String k:tradingDataMap.keySet()){
+            timeSeries.add(tradingDataMap.get(k).setDate(ZonedDateTime.parse(k+" "+metaData.getTimeZone(),formatter)));
+        }
+        return this;
     }
 
     public MetaData getMetaData() {
         return metaData;
     }
 
-    public Map<String,TradingData> getTimeSeries() {
+    public ArrayList<TradingData> getTimeSeries() {
         return timeSeries;
     }
 
     @Override public String toString(){
         StringBuilder sb = new StringBuilder(metaData.toString());
-        for (String k : timeSeries.keySet()){
-            sb.append("\n").append(k).append("\n").append(timeSeries.get(k));
+        if (timeSeries == null) {
+            for (String k : tradingDataMap.keySet()) {
+                sb.append("\n").append(k).append("\n").append(tradingDataMap.get(k));
+            }
+        }else {
+            for (TradingData td : timeSeries) {
+                sb.append("\n").append(td);
+            }
         }
         return sb.toString();
     }
