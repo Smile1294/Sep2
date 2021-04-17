68328325802d4bdc3ffae0b577a0a887a8bc5f00
Kim Tranberg <chooseanother@users.noreply.github.com>
2021-04-17 10:17:22 +0200

    Update ViewModelFactory.java
    messy mess

--- Server/src/viewmodel/ViewModelFactory.java
+++ Server/src/viewmodel/ViewModelFactory.java
@@ -8,8 +8,12 @@ public class ViewModelFactory {
     private PlaceOrderViewModel placeOrderController;
     private LoginViewModel loginViewModel;
     private RegisterViewModel registerViewModel;
+    private AccountViewModel accountViewModel;
+    private TransferCashViewModel transferCashViewModel;
+    
     public ViewModelFactory(Model model) throws IOException {
         UserInformation userInformation = new UserInformation();
+        TransferState transferState = new TransferState();
         this.placeOrderController = new PlaceOrderViewModel(model);
         this.loginViewModel = new LoginViewModel(model,userInformation);
         this.registerViewModel = new RegisterViewModel(model,userInformation);
