1e018d58aa85cff17795575824f34873f3bbf443
Luka69-420 <lukaudovicic10@gmail.com>
2021-05-05 14:47:38 +0200

    added javadocs to the user, userlist and username classes

--- Server/src/model/User.java
+++ Server/src/model/User.java
@@ -1,5 +1,9 @@
 package model;
 
+/**
+ * User class represents a user
+ */
+
 public class User {
     private Balance balance;
     private UserName userName;
@@ -8,14 +12,13 @@ public class User {
     private Stocks stocks;
 
     /**
+     * A constructor which is setting instance variables for logging in.
      *
-     *
-     * @param userName
-     * @param password
+     * @param userName userName of the user
+     * @param password password of the user
      */
     public User(UserName userName, Password password) {
-        if (userName == null || password == null)
-        {
+        if (userName == null || password == null) {
             throw new IllegalArgumentException("Null username or password");
         }
         this.userName = userName;
@@ -26,20 +29,22 @@ public class User {
     }
 
     /**
+     * A constructor which is setting instance variables for registering a user
      *
-     * @param userName
-     * @param password
-     * @param passwordConfirm
+     * @param userName        wanted userName of the user
+     * @param password        wanted password of the user
+     * @param passwordConfirm conformation of password
+     * @param email           users email
+     * @param emailConfirm    conformation of the email
      */
     public User(UserName userName, Password password, Password passwordConfirm, Email email, Email emailConfirm) {
-        if (userName == null || password == null || passwordConfirm == null)
-        {
+        if (userName == null || password == null || passwordConfirm == null) {
             throw new IllegalArgumentException("Null username or password");
         }
-        if (!password.equals(passwordConfirm)){
+        if (!password.equals(passwordConfirm)) {
             throw new IllegalArgumentException("Passwords does not match");
         }
-        if (!email.toString().equals(emailConfirm.toString())){
+        if (!email.toString().equals(emailConfirm.toString())) {
             throw new IllegalArgumentException("Emails does not match");
         }
         this.userName = userName;
@@ -48,24 +53,30 @@ public class User {
         this.balance = new Balance();
         this.stocks = new Stocks();
     }
+
     /**
+     * Buys stock
      *
-     * @void
+     * @param stock the stock that is being bought
      */
-    public void BuyStock(Stock stock){
+    public void BuyStock(Stock stock) {
 
         stocks.addStock(stock);
     }
+
     /**
+     * Sells a stock
      *
-     * @void
+     * @param stock the stock that is being sold
      */
-    public void SellStock(Stock stock){
+    public void SellStock(Stock stock) {
         stocks.addStock(stock);
     }
+
     /**
+     * gets the stock
      *
-     * @Stocks
+     * @return a stock
      */
 
     public Stocks getStocks() {
@@ -73,68 +84,92 @@ public class User {
     }
 
     /**
+     * gets the balance
      *
-     * @return
+     * @return a balace
      */
     public Double getBalance() {
         return balance.getBalance();
     }
 
     /**
+     * gets the userName
      *
-     * @return
+     * @return userName
      */
     public UserName getUserName() {
         return userName;
     }
 
     /**
+     * gets the password
      *
-     * @return
+     * @return password
      */
     public Password getPassword() {
         return password;
     }
 
     /**
+     * adds the amount
      *
-     * @param amount
+     * @param amount amount that is being added
+     *
      */
-    public void addCash(double amount){
+    public void addCash(double amount) {
         balance.add(amount);
     }
 
     /**
+     * withdraws the amount
      *
-     * @param amount
+     * @param amount amount that is being withdrawn
      */
-    public void withDraw(double amount){
+    public void withDraw(double amount) {
         balance.withDraw(amount);
     }
 
-    public boolean UserOwnStock(Stock stock)
-    {
-        for(Stock stock1: stocks.getAllStocks())
-        {
-            if(stock1.equals(stock))
+    /**
+     * goes thro the stocks and checks if the user holds a stock
+     *
+     * @param stock the stock that is being checked
+     * @return returns true if the user holds a stock , else it returns false
+     */
+
+    public boolean UserOwnStock(Stock stock) {
+        for (Stock stock1 : stocks.getAllStocks()) {
+            if (stock1.equals(stock))
                 return true;
         }
         return false;
     }
 
+    /**
+     * gets a email
+     *
+     * @return returns an email
+     *
+     */
 
     public Email getEmail() {
         return email;
     }
 
-    @Override public boolean equals(Object o){
-        if (o == null){
+    /**
+     *  comparing an object to the user
+     * @param o the object that is getting compared with
+     * @return returns userName and password
+     */
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
             return false;
         }
-        if (o == this){
+        if (o == this) {
             return true;
         }
-        if (o instanceof User){
+        if (o instanceof User) {
             User other = (User) o;
             return userName.equals(other.userName) && password.equals(other.password);
         }
--- Server/src/model/UserList.java
+++ Server/src/model/UserList.java
@@ -1,85 +1,140 @@
 package model;
 
-
-import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserList
-{
-  private List<User> users;
+/**
+ * UserList class represents a users in a list
+ */
 
-  public UserList()
-  {
-    users = new ArrayList<>();
-  }
+public class UserList {
+    private List<User> users;
 
-  public User getUser(UserName userName){
-    for (User u : users){
-      if (u.getUserName().equals(userName)){
-        return u;
-      }
+    /**
+     * A constructor which is initialising arraylist of type User
+     */
+
+    public UserList() {
+        users = new ArrayList<>();
     }
-    return null;
-  }
 
-  public boolean addUser(User user) throws Exception {
-    if (nameExist(user.getUserName())){
-      throw new Exception("Username already exists");
+    /**
+     * Finds a user from a user list
+     *
+     * @param userName userName of the user
+     * @return userName of the user
+     */
+
+    public User getUser(UserName userName) {
+        for (User u : users) {
+            if (u.getUserName().equals(userName)) {
+                return u;
+            }
+        }
+        return null;
     }
-    users.add(user);
-    return true;
-  }
 
-  public boolean nameExist(UserName userName){
-    for(User x: users){
-      if(x.getUserName().equals(userName)){
+    /**
+     * @param user new User that is added to the list
+     * @return returns true
+     * @throws Exception if the user isn't found
+     */
+
+    public boolean addUser(User user) throws Exception {
+        if (nameExist(user.getUserName())) {
+            throw new Exception("Username already exists");
+        }
+        users.add(user);
         return true;
-      }
     }
-    return false;
-  }
 
-  public boolean userExist(User user){
-    return users.contains(user);
-  }
+    /**
+     * checks if the userName  exists
+     *
+     * @param userName userName of the user
+     * @return returns true
+     */
 
-  public double getBalance(UserName userName){
-    for (User u : users){
-      if (u.getUserName().equals(userName)){
-        return u.getBalance();
-      }
+    public boolean nameExist(UserName userName) {
+        for (User x : users) {
+            if (x.getUserName().equals(userName)) {
+                return true;
+            }
+        }
+        return false;
     }
-    return 0.0;
-  }
-  public Stocks getStocks(UserName userName){
-    for (User u : users){
-      if (u.getUserName().equals(userName)){
-        return u.getStocks();
-      }
-    }
-    return null;
-  }
 
-  public void transferMoney(UserName userName, double amount, boolean isWithdraw){
-    User user = null;
-    for (User u : users){
-      if (u.getUserName().equals(userName)){
-        user = u;
-      }
-    }
-    if (isWithdraw){
-      user.withDraw(amount);
-    }
-    else {
-      user.addCash(amount);
-    }
-  }
+    /**
+     * Checks if the user exists
+     * @param user User that is getting checked
+     * @return user
+     */
 
-  @Override
-  public String toString() {
-    return "UserList{" +
-            "users=" + users +
-            '}';
-  }
+    public boolean userExist(User user) {
+        return users.contains(user);
+    }
+
+    /**
+     * Getting the user balance
+     * @param userName Username of the user
+     * @return users balance
+     */
+
+    public double getBalance(UserName userName) {
+        for (User u : users) {
+            if (u.getUserName().equals(userName)) {
+                return u.getBalance();
+            }
+        }
+        return 0.0;
+    }
+
+    /**
+     * Getting the user stocks
+     * @param userName Username of the user
+     * @return users stocks
+     */
+
+    public Stocks getStocks(UserName userName) {
+        for (User u : users) {
+            if (u.getUserName().equals(userName)) {
+                return u.getStocks();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Withdrawing or depositing money
+     * @param userName Username of the user that is transferring money
+     * @param amount amount that is getting transferred
+     * @param isWithdraw
+     */
+
+    public void transferMoney(UserName userName, double amount, boolean isWithdraw) {
+        User user = null;
+        for (User u : users) {
+            if (u.getUserName().equals(userName)) {
+                user = u;
+            }
+        }
+        if (isWithdraw) {
+            user.withDraw(amount);
+        } else {
+            user.addCash(amount);
+        }
+    }
+
+    /**
+     * toString version of the users
+     *
+     * @return users
+     */
+
+    @Override
+    public String toString() {
+        return "UserList{" +
+                "users=" + users +
+                '}';
+    }
 }
