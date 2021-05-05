4378c1b99a92e7ca32456665056d03dd20b5800b
Luka69-420 <lukaudovicic10@gmail.com>
2021-05-05 16:26:22 +0200

    added javadocs to the email, password & status class

--- Server/src/model/Status.java
+++ Server/src/model/Status.java
@@ -1,13 +1,27 @@
 package model;
 
+/**
+ * Status class is representing statuses of the order
+ */
+
 public enum Status {
     OPEN("Open"),CLOSED("Closed"),COMPLETED("Completed");
 
     private String status;
 
+    /**
+     * constructor setting the instance variable
+     * @param status status of the stock
+     */
+
     private Status(String status){
         this.status = status;
     }
+
+    /**
+     * getting the status
+     * @return status
+     */
 
     public String getStatus() {
         return status;
