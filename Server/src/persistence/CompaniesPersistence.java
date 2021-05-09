dc981a2d9382f863a00ac9e6b6293f384ca8b35f
Luka69-420 <lukaudovicic10@gmail.com>
2021-05-09 18:54:43 +0200

    added javadocs to the utility & persistence model classes

--- Server/src/persistence/CompaniesPersistence.java
+++ Server/src/persistence/CompaniesPersistence.java
@@ -2,8 +2,25 @@ package persistence;
 
 import model.Companies;
 
+/**
+ * CompaniesPersistence is interface for loading and saving companies
+ */
 
 public interface CompaniesPersistence {
+
+    /**
+     * saving companies list into a file
+     * @param companiesList list of companies that is being stored
+     * @param filename name of a file that is information stored on
+     */
+
     void save(Companies companiesList, String filename);
+
+    /**
+     * loading companies list from a file
+     * @param fileName from file to load
+     * @return companies list
+     */
+
     Companies load(String fileName);
 }
--- Server/src/persistence/OrdersPersistence.java
+++ Server/src/persistence/OrdersPersistence.java
@@ -3,7 +3,27 @@ package persistence;
 import model.Orders;
 import model.UserList;
 
+
+/**
+ * OrdersPersistence is interface for loading and saving orders
+ */
+
 public interface OrdersPersistence {
+
+    /**
+     * saving orders list into a file
+     * @param ordersList list of orders that is being stored
+     * @param filename name of a file that is information stored on
+     */
+
+
     void save(Orders ordersList, String filename);
+
+    /**
+     * loading orders list from a file
+     * @param fileName from file to load
+     * @return order list
+     */
+
     Orders load(String fileName);
 }
