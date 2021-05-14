ab5045e73985c37b1a9e4322271a66ed3562a75a
chooseanother <kim.tranberg@gmail.com>
2021-05-14 15:00:54 +0200

    created stockAPI package

---
+++ Server/src/stockAPI/APIKey.java
@@ -0,0 +1,20 @@
+package stockAPI;
+
+// Kim "1Z19I81E7YM3VHW9"
+// Dávid "JI2Z4NFER4CQ52LP","0K7TBQTPK03XHNB0","TD4H3494BLXYYBE1","408T2WU2BSBT1EP1"
+// Luka "GB6KGRHXGIJNGXU5"
+//
+//
+
+public class APIKey {
+    private static final String[] KEYS = {"1Z19I81E7YM3VHW9","JI2Z4NFER4CQ52LP","0K7TBQTPK03XHNB0","TD4H3494BLXYYBE1",
+            "408T2WU2BSBT1EP1","GB6KGRHXGIJNGXU5"};
+    private static int index = 4;
+
+    public static String getKey(){
+        if (index>KEYS.length-1){
+            index = 0;
+        }
+        return KEYS[index++];
+    }
+}
---
+++ Server/src/stockAPI/RequestType.java
@@ -0,0 +1,30 @@
+package stockAPI;
+
+/**
+ * Request type is a class representing request type from the api
+ */
+
+public enum RequestType {
+    INTRADAY("TIME_SERIES_INTRADAY"),DAILY("TIME_SERIES_DAILY"),WEEKLY("TIME_SERIES_WEEKLY"),
+    MONTHLY("TIME_SERIES_MONTHLY"),;
+
+    private String requestType;
+
+    /**
+     *  Constructor that is initialising instance variable
+     * @param requestType the type that is requested
+     */
+
+    private RequestType(String requestType){
+        this.requestType = requestType;
+    }
+
+    /**
+     * getting request type
+     * @return request type
+     */
+
+    public String getRequestType(){
+        return requestType;
+    }
+}
